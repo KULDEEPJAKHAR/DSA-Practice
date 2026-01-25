8a7d8e677825301b744548cbccb59066829b414d
KULDEEPJAKHAR <kuldeepjakhar8055@gmail.com>
2026-01-25 22:00:25 +0530

    Add program to calculate factorial

---
+++ factorial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    int fact = 1;
+    for(int i = 1;i<=n;i++){
+        fact*=i;
+
+    }
+    return fact;
+};
+int main(){
+    int n;
+    cout<<"Enter a number to print its factorial: "<<endl;
+    cin>>n;
+    cout<<factorial(n)<<endl;
+    return 0;
+    
+}
