58c6114c8eb725b0573a7361e6163ac06d2537d6
KULDEEPJAKHAR <kuldeepjakhar8055@gmail.com>
2026-01-28 09:46:41 +0530

    Insertion sort

---
+++ Insertion_sort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {1,3,5,4,2};
+    int n = 5;
+    for(int i = 1;i<n;i++){
+     int curr = arr[i];
+     int prev = i-1;
+     while(prev>=0 &&arr[prev]>curr){
+        arr[prev+1] = arr[prev];
+        prev--;
+     }
+     arr[prev+1]=curr;
+
+    }
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" " ;
+    }
+    return 0;
+}
