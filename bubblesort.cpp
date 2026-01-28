6c692d66880574d25e3cfbfdb92711c2f7c63de8
KULDEEPJAKHAR <kuldeepjakhar8055@gmail.com>
2026-01-28 09:43:14 +0530

    Bubble sort

---
+++ bubblesort.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    int arr[5] = {4,6,3,5,1};
+    for(int i = 0;i<n-1;i++){
+        for(int j = 0;j<n-i-1;j++){
+            if(arr[j]<arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;  
+}
