07351cb5ba236c592bee6a2d520a36d1bd0edb55
KULDEEPJAKHAR <kuldeepjakhar8055@gmail.com>
2026-01-31 21:59:22 +0530

    Binary

---
+++ binary.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int binarySearch(vector<int> &nums,int target){
+    int left = 0;
+    int right = nums.size()-1;
+    while(left<=right){
+        int mid = left+(right-left)/2;
+    
+    if(nums[mid]== target){
+        return mid;
+
+    }
+    else if(nums[mid]<=target){
+        left = mid+1;
+    }
+    else{
+        right = mid-1;
+
+    }}
+    return -1;
+}
+int main(){
+    vector<int> nums = {1,3,5,7,9};
+    int target = 5;
+    int ans = binarySearch(nums,target);
+    cout<<ans;
+    return 0;
+}
